e4273ef14642161b6e55c8e79c79847b285ded1c
Laptop 91 <ddfd@>
2026-09-03 00:43:30 +0300

    pozitii poznase in teren

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Auto_Albastru.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Auto_Albastru.java
@@ -48,10 +48,10 @@ public class Auto_Albastru extends OpMode {
     public DcMotorEx TURELA = null;
     public DcMotor LIFT = null;
     public DcMotor INTAKE=null;
-    private final Pose startPose = new Pose(20,123,Math.toRadians(140));
-    private final Pose shootPose = new Pose(59,84,Math.toRadians(180));
-    private final Pose GateTake=new Pose(10,65,Math.toRadians(150));
-    private final Pose Stem =new Pose(40,15,Math.toRadians(180));
+    private final Pose startPose = new Pose(180,12,Math.toRadians(90));
+    private final Pose shootPose = new Pose(59,84,Math.toRadians(0));
+    private final Pose GateTake=new Pose(10,65,Math.toRadians(0));
+    private final Pose Stem =new Pose(40,15,Math.toRadians(-90));
     private final Pose[] Parcari ={
             new Pose(60,60,Math.toRadians(0)),  //case 1
             new Pose(108,60,Math.toRadians(0)),  //case 2
